99afd4cb15d6c4d42e32cf3a4cecd6afd8026814
Karani Wachira <wachirakeith@gmail.com>
2023-05-16 13:05:36 +0300

    worked on the lastElement & nestedLoops questions

--- nestedLoops.js
+++ nestedLoops.js
@@ -12,15 +12,15 @@
 // ------
 // ------
 
-// let k = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+let k = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
 
-// for (let i = 0; i < k.length; i++) {
-//     console.log(`row ${i}`);
-// for (let j = 0; j < k[i].length; j++) {
-// console.log(` ${k[i][j]}`);
-// }
-// console.log("------");
-// }
+for (let i = 0; i < k.length; i++) {
+    console.log(`row ${i}`);
+for (let j = 0; j < k[i].length; j++) {
+console.log(` ${k[i][j]}`);
+}
+console.log("------");
+}
 
 
 
